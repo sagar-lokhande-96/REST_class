958f61e180bd33dd7f8495e6e1c26186462bf6b1
Sagar Lokhande <sagarlokhande159@gmail.com>
2025-03-04 20:42:32 +0530

    solve some bassic error

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ app.post("/posts",(req,res)=>{
 app.get("/posts/:id",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p)=> id === p.id);
-    console.log(id);
     res.render("show.ejs", {post} );
 });
 
